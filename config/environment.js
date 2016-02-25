a1db2e4f9e7d0433f04cdb05b6e7edf60abffd5a
Santiago Bandiera <santiago@hanzo.es>
2016-02-25 19:48:47 +0100

    Generate the basic structure and isntall necesary libraries

--- config/environment.js
+++ config/environment.js
@@ -3,7 +3,10 @@
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'blog',
+    podModulePrefix: 'blog/features',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://YOUR-FIREBASE-NAME.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
