245b755c2dc5cab577b83008c85904a6567476ea
Santiago Bandiera <santiago@hanzo.es>
2016-02-26 18:07:25 +0100

    All the others things. Sorry

--- app/features/application/route.js
+++ app/features/application/route.js
@@ -1,4 +1,24 @@
 import Ember from 'ember';
+const { get } = Ember;
 
 export default Ember.Route.extend({
+  beforeModel() {
+    return get(this, 'session').fetch().catch(function () {});
+  },
+
+  model() {
+    return this.store.findAll('post');
+  },
+
+  actions:{
+    login() {
+      get(this, 'session').open('firebase', { provider: 'twitter' }).then(function (data) {
+        console.log(data);
+      });
+    },
+
+    logout() {
+      get(this, 'session').close();
+    },
+  },
 });
